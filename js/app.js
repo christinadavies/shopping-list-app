287c36f564278ddd0b8390f2df91d6a22083b83a
Christina Davies <cc.goulet@gmail.com>
2015-06-15 23:59:17 +0900

    commit message

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
-	// Global variables
-	var submissions = 0;
-	
-    // User can use "enter" key to work as a submit button
+    // User can press "enter" to submit new item
 	function getItem() {
 		$('input#add-items').keydown(function(e) {
 			if (e.keyCode == 13) {
@@ -11,11 +8,11 @@ $(document).ready(function() {
 		});
 	}
 	getItem();
-	// posts input from user
+	// Posts input from user
 	function postItem() {
 		var item = $('input#add-items').val();
-		var work = '<li class="item"><button class="done"><i class="fa fa-check"></i></button>' + item + '<button class="remove"><i class="fa fa-times"></i></button>' + '</li>';
-		$('.list-area').prepend(work);
+		var create = '<li class="item"><button class="done"><i class="fa fa-check"></i></button>' + item + '<button class="remove"><i class="fa fa-times"></i></button>' + '</li>';
+		$('.list-area').prepend(create);
 		$('input#add-items').val('');
 		$('.list-area li:first-child')
         .css("opacity", "0")
@@ -29,12 +26,12 @@ $(document).ready(function() {
             { queue: false, duration: 200 }
         );
 	}
-	// changes class of list item
+	// Changes class of list item
 	$('.list-area').on('click', '.done', function() {
 		$(this).closest('li').toggleClass('item item-bought');
 		$(this).remove();
 	});
-	// allows user to delete items
+	// Allows user to delete items
 	$('.list-area').on('click', '.remove', function() {
 		$(this).closest('li').fadeOut(200);
 	});
